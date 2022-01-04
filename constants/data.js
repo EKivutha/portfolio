71ce2109179dc5b0ac5703c7e827d41ffd514f0d
ezz <gerkibz@outlook.com>
2022-01-04 10:45:15 +0300

    First version

---
+++ constants/data.js
@@ -0,0 +1,60 @@
+const userData = {
+    githubUsername: "EKivutha",
+    name: "Ezra Kivutha",
+    designation:'Software| ML Engineer',
+    avatarUrl:'',
+    email:'mutunjau@gmail.com',
+    phone:'+254727214536',
+    address:'Nairobi,Kenya',
+    projects:[
+        {
+            title:'',
+            link:'',
+            imgUrl:'',
+        },
+        {
+            title:'',
+            link:'',
+            imgUrl:'',
+        },
+        
+    ],
+    about:{
+        title:'Software| ML Engineer',
+        description:[],
+        currentProject:'',
+        currentProjectUrl:'',
+    },
+    experience:[
+        {
+            title:'',
+            company:'',
+            year: '',
+            companyLink:'',
+            desc:'',
+        },
+        {
+            title:'',
+            company:'',
+            year: '',
+            companyLink:'',
+            desc:'',
+        },
+        {
+            title:'',
+            company:'',
+            year: '',
+            companyLink:'',
+            desc:'',
+        },
+    ],
+    resumeUrl:'',
+    socialLinks: {
+        instagram: "",
+        twitter: "",
+        linkedin: "",
+        github: "",
+        facebook: "",
+      },
+};
+export default userData;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require("tailwindcss/colors")
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      transitionDuration: {
+        0: "0ms",
+        2000: "2000ms",
+        4000: "4000ms",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+    display: ["responsive", "group-hover", "group-focus"]
+  },
+  plugins: [],
+}

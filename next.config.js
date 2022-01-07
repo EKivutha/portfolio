1255ba4762fcd3e4ab863757f606636ad8fa40b7
ezz <gerkibz@outlook.com>
2022-01-07 15:30:08 +0300

    gh update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 const debug = process.env.NODE_ENV !== "production";
+const withPlugins = require('next-compose-plugins');
+const basePath = ON_GITHUB_PAGES ? '/portfolio' : '';
+const assetPrefix = ON_GITHUB_PAGES ? '/porfolio/' : '';
 
 module.exports = {
   reactStrictMode: true,
   // distDir: './build',
   // outDir: './out'
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath,
+  assetPrefix,
   exportPathMap: function () {
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
     }
   },
+     
 
-
-  assetPrefix: !debug ? '/porfolio/' : '',
+// assetPrefix: !debug ? '/porfolio/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
